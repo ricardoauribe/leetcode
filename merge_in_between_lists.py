e4979397c653d6102686e408b0d467b27bace981
Ricardo Uribe <ricardoauribe@hotmail.com>
2021-04-01 21:26:46 -0600

    Approach based on node value

--- merge_in_between_lists.py
+++ merge_in_between_lists.py
@@ -11,30 +11,33 @@ class ListNode:
 		while self.next:
 			print(self.val)
 			self = self.next
+		print(self.val)
 
 
 class Solution:
 	def mergeInBetween(self, list1: ListNode, a: int, b: int, list2: ListNode) -> ListNode:
 
-		pointer1 = list1
-		pointer2 = list1
-		while pointer1.next and pointer1.val != a:
-			if pointer1.val == a:
-				pointer1.next = list2
-				while list2.next:
-					list2 =list2.next
-			else:
-				pointer1 = pointer1.next
-				pointer2 = pointer2.next
+		prev = None
+		curr = list1
+		head = curr
 
-		while pointer2.next and pointer2.val != b:
-			if pointer2.val == b:
-				list2.next = pointer2.next
-			else:
-				pointer2 = pointer2.next
+		while curr.next and curr.val != a:
+			prev = curr
+			curr = curr.next
+			
+		list1Pointer = prev.next
+		prev.next = list2
+		curr = list2
 
-		pointer2.printVals()
-		return list1
+		while curr.next:
+			prev = curr
+			curr = curr.next
+
+		while list1Pointer.next and list1Pointer.val != b:
+			list1Pointer = list1Pointer.next
+				
+		curr.next = list1Pointer.next
+		return head
 
 sol = Solution()
 list1 = ListNode(0, ListNode(1, ListNode(2, ListNode(3, ListNode(4,ListNode(5,None))))))
