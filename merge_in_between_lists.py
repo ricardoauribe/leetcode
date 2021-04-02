90db70a9f087246c7e9c4dc18b60b08b228cb471
Ricardo Uribe <ricardoauribe@hotmail.com>
2021-04-01 20:50:37 -0600

    print function

--- merge_in_between_lists.py
+++ merge_in_between_lists.py
@@ -7,12 +7,33 @@ class ListNode:
 		self.val = val
 		self.next = next
 
+	def printVals(self):
+		while self.next:
+			print(self.val)
+			self = self.next
+
 
 class Solution:
 	def mergeInBetween(self, list1: ListNode, a: int, b: int, list2: ListNode) -> ListNode:
-		while list1.next and list1.val != a:
-			print(list1.val)
-			list1 = list1.next
+
+		pointer1 = list1
+		pointer2 = list1
+		while pointer1.next and pointer1.val != a:
+			if pointer1.val == a:
+				pointer1.next = list2
+				while list2.next:
+					list2 =list2.next
+			else:
+				pointer1 = pointer1.next
+				pointer2 = pointer2.next
+
+		while pointer2.next and pointer2.val != b:
+			if pointer2.val == b:
+				list2.next = pointer2.next
+			else:
+				pointer2 = pointer2.next
+
+		pointer2.printVals()
 		return list1
 
 sol = Solution()
